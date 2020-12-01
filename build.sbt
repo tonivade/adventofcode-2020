2a9500c86bc60cf3d4b6476fa0527808e3de8455
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-12-01 11:26:55 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "adventofcode-2020"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.12.12"
---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,29 @@
+package adventofcode
+
+import scala.io.Source
+
+object Day1Util {
+   
+  val numbers = Source.fromFile("src/main/resources/input.txt").getLines().map(_.toInt).toList
+
+  def combinations(number: Int, sum: Int): List[Int] = 
+    numbers.combinations(number).find(x => x.fold(0)(_ + _) == sum).orNull
+}
+
+object Day1Part1 extends App {
+
+  println("Day1Part1")
+
+  val result = Day1Util.combinations(2, 2020)
+
+  println(s"values ${result(0)} and ${result(1)} = ${result(0) * result(1)}")
+}
+
+object Day1Part2 extends App {
+
+  println("Day1Part2")
+
+  val result = Day1Util.combinations(3, 2020)
+
+  println(s"values ${result(0)} and ${result(1)} and ${result(2)} = ${result(0) * result(1) * result(2)}")
+}
