04f557612063098833d61f18725b628eb03a8873
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-12-05 11:02:37 +0100

    some code improvements

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -7,23 +7,26 @@ object Day5 {
   def calculateSeat(line: String): (Int, Int) = {
     val (row, col) = line.splitAt(7)
 
-    val (rmin, rmax) = search(0, 127, row, 'F') 
-    val (cmin, cmax) = search(0, 7, col, 'L') 
+    val r = search(row, 'F') 
+    val c = search(col, 'L') 
 
-    assert(rmin == rmax)
-    assert(cmin == cmax)
-    (rmin, cmin)
+    (r, c)
   }
 
-  def search(start: Int, end: Int, line: String, lo: Char): (Int, Int) = 
-    line.foldLeft((start, end))((state, c) => {
-      val (min, max) = state
-      val half = ((max - min) / 2) + 1
-      if (c == lo)
-        (min, max - half)
-      else
-        (min + half, max)
-    })
+  def search(line: String, lo: Char): Int = {
+    def decode(line: String, lo: Char): (Int, Int) = {
+      line.foldLeft((0, (1 << line.size) - 1))((state, c) => {
+        val (min, max) = state
+        val half = ((max - min) / 2) + 1
+        if (c == lo)
+          (min, max - half)
+        else
+          (min + half, max)
+      })
+    } ensuring(r => r._1 == r._2)
+    
+    decode(line, lo)._1
+  }
 
   def seatId(seat: (Int, Int)) = (seat._1 * 8) + seat._2
   
