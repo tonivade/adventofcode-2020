434735bb07553cbe4fd802a097c653bfb9f6810c
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-12-05 09:38:16 +0100

    using fold instead of ugly for

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -7,27 +7,23 @@ object Day5 {
   def calculateSeat(line: String): (Int, Int) = {
     val (row, col) = line.splitAt(7)
 
-    val (rmin, rmax) = search(0, 127, row, 'F', 'B') 
-    val (cmin, cmax) = search(0, 7, col, 'L', 'R') 
+    val (rmin, rmax) = search(0, 127, row, 'F') 
+    val (cmin, cmax) = search(0, 7, col, 'L') 
 
     assert(rmin == rmax)
     assert(cmin == cmax)
     (rmin, cmin)
   }
 
-  def search(start: Int, end: Int, line: String, lo: Char, up: Char): (Int, Int) = {
-    var min = start
-    var max = end
-    for (c <- line) {
+  def search(start: Int, end: Int, line: String, lo: Char): (Int, Int) = 
+    line.foldLeft((start, end))((state, c) => {
+      val (min, max) = state
       val half = ((max - min) / 2) + 1
-      if (c == lo) {
-        max = max - half 
-      } else if (c == up) {
-        min = min + half
-      } else throw new IllegalArgumentException(s"invalid char $c")
-    }
-    (min, max)
-  }
+      if (c == lo)
+        (min, max - half)
+      else
+        (min + half, max)
+    })
 
   def seatId(seat: (Int, Int)) = (seat._1 * 8) + seat._2
   
