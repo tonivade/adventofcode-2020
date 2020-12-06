40b05b533fb76601a2e905375e1680af401b2192
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-06 10:50:03 +0100

    Day6 part2 solution

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -4,16 +4,19 @@ import scala.io.Source
 
 object Day6 {
 
-  def parseGroup(input: String): String =
-    input.trim().split('\n').mkString.toSet.mkString
+  def union(input: Array[Set[Char]]): String =
+    input.reduce((a, b) => a.union(b)).mkString
+
+  def intersect(input: Array[Set[Char]]): String =
+    input.reduce((a, b) => a.intersect(b)).mkString
 
   val input = Source.fromFile("src/main/resources/customs.txt").mkString
 
-  def parse(answers: String) = 
-    answers.split("\\n\\n").map(parseGroup)
+  def parse(answers: String, parser: Array[Set[Char]] => String) = 
+    answers.split("\\n\\n").map(_.trim()).map(_.split('\n').map(_.toSet)).map(parser)
 
-  def count(answers: String) =
-    parse(answers).map(_.size).sum
+  def count(answers: String, parser: Array[Set[Char]] => String) =
+    parse(answers, parser).map(_.size).sum
 }
 
 object Day6Part1 extends App {
@@ -21,7 +24,15 @@ object Day6Part1 extends App {
 
   println("Day6 Part1")
 
-  println(count(input))
+  println(count(input, union))
+}
+
+object Day6Part2 extends App {
+  import Day6._
+
+  println("Day6 Part2")
+
+  println(count(input, intersect))
 }
 
 object Day6Test extends App {
@@ -44,5 +55,6 @@ object Day6Test extends App {
                   |b
                   """.stripMargin
 
-  assert(count(testInput) == 11)
+  assert(count(testInput, union) == 11)
+  assert(count(testInput, intersect) == 6)
 }
