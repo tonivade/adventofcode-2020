bed9b778a9189a5b2444b5ea294af7e2bb4efdcf
Antonio Muñoz <antonio.munoz@clarity.ai>
2020-12-09 08:09:28 +0100

    Day9 Part1 solution

---
+++ src/main/scala/Day9.scala
@@ -0,0 +1,57 @@
+package adventofcode
+
+import scala.io.Source
+
+object Day9 {
+  def isValid(numbers: Array[Long]): Boolean = {
+    val last = numbers.last
+    val preamble = numbers.dropRight(1)
+    preamble.combinations(2).map(x => x(0) + x(1)).exists(_ == last)
+  }
+
+  def findInvalid(input: Array[Long], preambleSize: Int): List[Long] =
+    input.sliding(preambleSize + 1).filterNot(isValid).map(_.last).toList
+}
+
+object Day9Part1 extends App {
+  import Day9._
+
+  println("Day9 Part1")
+
+  val input = Source.fromFile("src/main/resources/encrypted.txt").getLines().map(_.toLong).toArray
+
+  println(findInvalid(input, 25).head)
+}
+
+object Day9Test extends App {
+  import Day9._
+
+  val input = """35
+                |20
+                |15
+                |25
+                |47
+                |40
+                |62
+                |55
+                |65
+                |95
+                |102
+                |117
+                |150
+                |182
+                |127
+                |219
+                |299
+                |277
+                |309
+                |576""".stripMargin
+
+  val encrypted = input.split('\n').map(_.toLong)
+
+  val result = findInvalid(encrypted, 5)
+
+  assert(result.head == 127)
+
+  println("OK")
+}
