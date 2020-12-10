20375acf36af5eada6a2eafe8dd51f93ff0725c8
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-11 00:57:47 +0100

    Day10 part2 solution

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -4,14 +4,35 @@ import scala.io.Source
 
 object Day10 {
 
-  def findAdapters(adapters: List[Int]): Map[Int, Int] = {
+  def find(adapters: List[Int]): List[(Int, List[Int])] = {
     val sortedAdapters = 0 :: adapters.sorted
-    sortedAdapters.sorted
-      .map(i => (i, sortedAdapters.filter(_ > i).filter(_ < i + 4).headOption.getOrElse(i + 3)))
+    sortedAdapters
+      .map(i => (i, sortedAdapters.filter(_ > i).filter(_ < i + 4)))
+  }
+
+  def findAdapters(adapters: List[Int]): Map[Int, Int] =
+    find(adapters)
+      .map(t => (t._1, t._2.headOption.getOrElse(t._1 + 3)))
       .map(t => t._2 - t._1)
       .groupBy(identity)
       .map(t => (t._1, t._2.size))
       .toMap
+
+  def arrangeAdapters(adapters: List[Int]): Set[Set[Int]] = {
+    val all = find(adapters).map(_._2)
+
+    val acc = all.head.map(Set(_)).toSet
+
+    all.tail.foldLeft(acc)((a, e) =>
+      e match {
+        case Nil => a
+        case _ => 
+          for { 
+            x <- a 
+            y <- e
+          } yield (x + y)
+      }
+    )
   }
 
   var input = Source.fromResource("jolts.txt").getLines().map(_.toInt).toList
@@ -22,28 +43,37 @@ object Day10Part1 extends App {
 
   println("Day10 Part1")
 
-  println(findAdapters(input).values.foldLeft(1)(_ * _))
+  println(findAdapters(input).values.product)
+}
+
+object Day10Part2 extends App {
+  import Day10._
+
+  println("Day10 Part2")
+
+  println(arrangeAdapters(input).size)
 }
 
 object Day10Test extends App {
   import Day10._
   
-  var input = """16
-                |10
-                |15
-                |5
-                |1
-                |11
-                |7
-                |19
-                |6
-                |12
-                |4""".stripMargin.linesIterator.map(_.toInt).toList
+  var input1 = """16
+                 |10
+                 |15
+                 |5
+                 |1
+                 |11
+                 |7
+                 |19
+                 |6
+                 |12
+                 |4""".stripMargin.linesIterator.map(_.toInt).toList
 
-  val result1 = findAdapters(input)
+  val result1 = findAdapters(input1)
 
   assert(result1(1) == 7)
   assert(result1(3) == 5)
+  assert(arrangeAdapters(input1).size == 8)
 
   val input2 = """28
                  |33
@@ -81,6 +111,7 @@ object Day10Test extends App {
 
   assert(result2(1) == 22)
   assert(result2(3) == 10)
+  assert(arrangeAdapters(input2).size == 19208)
 
   println("OK")
 }
