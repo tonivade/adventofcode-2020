57765d2daf073ae6d1ed89b84e955c4b3487f053
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-19 17:04:52 +0100

    Day19 part2 WIP

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -39,14 +39,16 @@ object Day19 {
   case class OrRule(left: Rule, right: Rule) extends Rule {
     override def applyTo(line: String): (Int, Boolean) = {
       val (consumedLeft, resultLeft) = left.applyTo(line)
-      val (consumedRight, resultRight) = right.applyTo(line)
 
       if (resultLeft)
-        (consumedLeft, resultLeft)
-      else if (resultRight)
-        (consumedRight, resultRight)
-      else
-        (0, false)
+        (consumedLeft, true)
+      else {
+        val (consumedRight, resultRight) = right.applyTo(line)
+        if (resultRight)
+          (consumedRight, true)
+        else
+          (0, false)
+      }
     }
   }
 
@@ -70,16 +72,22 @@ object Day19 {
         (1, true)
   }
 
-  def parseRule(rules: => Rules)(line: String): (Int, Rule) = {
+  def parseLine(line: String): (Int, String) = {
     val Array(pos, rule) = line.split(":")
+    
+    (pos.toInt, rule.trim())
+  }
 
+  def parseRule(rules: => Rules)(line: String): Rule = {
     val simpleRuleRegex = """"(\w)"""".r
     val refRegex = """(\d+)""".r
+    val refOrRegex = """(\d+) \| (\d+) (\d+)""".r
     val andRuleRegex = """(\d+) (\d+)""".r
     val orRuleRegex = """(\d+) \| (\d+)""".r
     val andOrAndRuleRegex = """(\d+) (\d+) \| (\d+) (\d+)""".r
+    val andOrAndAndRuleRegex = """(\d+) (\d+) \| (\d+) (\d+) (\d+)""".r
 
-    val result = rule.trim match {
+    val result = line match {
       case simpleRuleRegex(value) => SimpleRule(value.charAt(0))
       case andRuleRegex(a, b) => 
         AndRule(LazyRule(rules(a.toInt)), LazyRule(rules(b.toInt)))
@@ -89,20 +97,42 @@ object Day19 {
         OrRule(
           AndRule(LazyRule(rules(a.toInt)), LazyRule(rules(b.toInt))), 
           AndRule(LazyRule(rules(c.toInt)), LazyRule(rules(d.toInt))))
+      case refOrRegex(a, b, c) => 
+        OrRule(
+          LazyRule(rules(a.toInt)), 
+          AndRule(LazyRule(rules(b.toInt)), LazyRule(rules(b.toInt))))
       case refRegex(a) => LazyRule(rules(a.toInt))
-      case _ => throw new IllegalArgumentException(s"`${rule.trim}`")
+      case andOrAndAndRuleRegex(a, b, c, d, e) => 
+        OrRule(
+          AndRule(LazyRule(rules(a.toInt)), LazyRule(rules(b.toInt))), 
+          AndRule(LazyRule(rules(c.toInt)), 
+            AndRule(LazyRule(rules(d.toInt)), LazyRule(rules(e.toInt)))))
+      case _ => throw new IllegalArgumentException(s"`$line`")
     }
 
-    (pos.toInt, result)
+    result
   }
+
+  def parse(input: String): Map[Int, String] =
+    input.linesIterator.map(parseLine).toMap
+
+  val Array(rulesPart, linesPart) = Source.fromResource("rules.txt").mkString.split("\\n\\n")
 }
 
 object Day19Part1 extends App {
   import Day19._
 
-  val Array(rulesPart, linesPart) = Source.fromResource("rules.txt").mkString.split("\\n\\n")
+  val rules: Rules = Rules(parse(rulesPart).mapValues(parseRule(rules)).toMap)
 
-  val rules: Rules = Rules(rulesPart.linesIterator.map(parseRule(rules)).toMap)
+  println(linesPart.linesIterator.filter(rules.applyTo(0)).size)
+}
+
+object Day19Part2 extends App {
+  import Day19._
+
+  val input = parse(rulesPart)
+  val fixedInput = input + (8 -> "42 | 42 8") + (11 -> "42 31 | 42 11 31")
+  val rules: Rules = Rules(fixedInput.mapValues(parseRule(rules)).toMap)
 
   println(linesPart.linesIterator.filter(rules.applyTo(0)).size)
 }
@@ -138,12 +168,12 @@ object Day19Test extends App {
                 |2: 1 3 | 3 1
                 |3: "b"""".stripMargin
 
-  val rules: Rules = Rules(input.linesIterator.map(parseRule(rules)).toMap)
+  val rules: Rules = Rules(parse(input).mapValues(parseRule(rules)).toMap)
 
-  println(rules.applyTo("aab"))
-  println(rules.applyTo("aba"))
-  println(rules.applyTo("aa"))
-  println(rules.applyTo("ab"))
+  assert(rules.applyTo("aab") == List(0))
+  assert(rules.applyTo("aba") == List(0))
+  assert(rules.applyTo("aa").isEmpty)
+  assert(rules.applyTo("ab") == List(2))
 
   println("OK")
 }
