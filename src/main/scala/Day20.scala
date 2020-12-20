0d5f749a17a2dd62568acd322c45e4a038792e5d
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-20 16:49:48 +0100

    Day20 part1 solution

--- src/main/scala/Day20.scala
+++ src/main/scala/Day20.scala
@@ -9,6 +9,24 @@ object Day20 {
     def bottom: String = image.last
     def left: String = image.map(_.head).mkString
     def right: String = image.map(_.last).mkString
+
+    def rotate: Tile = Tile(id, rotate(image))
+    def flip: Tile = Tile(id, image.map(_.reverse))
+    def invert: Tile = Tile(id, invert(image))
+
+    private def rotate(input: Seq[String]): Seq[String] =
+      for {
+        i <- 0 until input(0).size
+      } yield (input.map(_(i)).mkString)
+
+    private def invert(input: Seq[String]): Seq[String] =
+      for {
+        i <- (input.size - 1) to 0 by -1
+      } yield (input(i))
+  }
+
+  case class Result(id: Int, top: Option[Int], botton: Option[Int], left: Option[Int], right: Option[Int]) {
+    def positives: Int = List(top,botton,left,right).flatMap(_.toList).size
   }
 
   def parse(input: String): Seq[Tile] = 
@@ -21,19 +39,46 @@ object Day20 {
   }
 
   def searchRight(tile: Tile, tiles: Seq[Tile]): Option[Int] = {
-    tiles.find(_.left == tile.right).map(_.id)
+    tiles.filterNot(_.id == tile.id).find(_.left == tile.right).map(_.id)
   }
 
   def searchLeft(tile: Tile, tiles: Seq[Tile]): Option[Int] = {
-    tiles.find(_.right == tile.left).map(_.id)
-  }
-
-  def searchTop(tile: Tile, tiles: Seq[Tile]): Option[Int] = {
-    tiles.find(_.bottom == tile.top).map(_.id)
+    tiles.filterNot(_.id == tile.id).find(_.right == tile.left).map(_.id)
   }
 
   def searchBottom(tile: Tile, tiles: Seq[Tile]): Option[Int] = {
-    tiles.find(_.top == tile.bottom).map(_.id)
+    tiles.filterNot(_.id == tile.id).find(_.top == tile.bottom).map(_.id)
+  }
+
+  def searchTop(tile: Tile, tiles: Seq[Tile]): Option[Int] = {
+    tiles.filterNot(_.id == tile.id).find(_.bottom == tile.top).map(_.id)
+  }
+
+  def findCorners(input: String): Seq[Int] = {
+    val tiles = parse(input)
+    val rotated = tiles.map(_.rotate)
+    val rnf = rotated.map(_.flip)
+    val flipped = tiles.map(_.flip)
+    val fnr = flipped.map(_.rotate)
+    val inverted = tiles.map(_.invert)
+    val inf = inverted.map(_.flip)
+    val infnr = inf.map(_.rotate)
+
+    val all = tiles ++ rotated ++ flipped ++ inverted ++ rnf ++ fnr ++ inf ++ infnr
+
+    val result = all.map { t => Result(
+      t.id,
+      searchTop(t, all), 
+      searchBottom(t, all), 
+      searchLeft(t, all),
+      searchRight(t, all))
+    }
+
+    result.groupBy(_.id)
+      .map(x => (x._1, x._2.maxBy(_.positives)))
+      .filter(_._2.positives == 2)
+      .map(_._1)
+      .toSeq
   }
 }
 
@@ -42,12 +87,28 @@ object Day20Part1 extends App {
 
   val input = Source.fromResource("tiles.txt").mkString
 
-  val tiles = parse(input)
-  println(tiles.size)
+  val corners = findCorners(input)
+
+  println(corners)
+  
+  println(corners.foldLeft(1L)(_ * _))
 }
 
 object Day20Test extends App {
   import Day20._
+
+  val x = Tile(1, List("TTT","...","BBB"))
+  val y = Tile(1, List("L.R","L.R","L.R"))
+
+  assert(x.top == "TTT")
+  assert(x.bottom == "BBB")
+  assert(y.left == "LLL")
+  assert(y.right == "RRR")
+
+  assert(x.rotate.left == x.top)
+  assert(x.rotate.right == x.bottom)
+  assert(y.flip.left == y.right)
+  assert(y.flip.right == y.left)
 
   val input = """Tile 2311:
                 |..##.#..#.
@@ -157,7 +218,11 @@ object Day20Test extends App {
                 |..#.......
                 |..#.###...""".stripMargin
 
-  val tiles = parse(input)
+  val corners = findCorners(input)
 
-  tiles.map(t => (t.id, searchTop(t, tiles), searchBottom(t, tiles), searchRight(t, tiles), searchLeft(t, tiles))).foreach(println)
+  assert(corners.size == 4)
+
+  assert(corners.foldLeft(1L)(_ * _) == 20899048083289L)
+
+  println("OK")
 }
