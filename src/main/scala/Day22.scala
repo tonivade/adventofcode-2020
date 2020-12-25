47721399b72178d196360507ba210f59d02cf53a
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-25 12:53:28 +0100

    Day22 par2 WIP

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -10,8 +10,10 @@ object Day22 {
   case class Player(deck: List[Card]) {
     def add(card1: Card, card2: Card) = Player(deck :+ card1 :+ card2)
     def left: Int = deck.size
+    def looser: Boolean = left == 0
     def take: (Player, Card) = (Player(deck.tail), deck.head)
     def points: Int = deck.reverse.zipWithIndex.map { case (card, pos) => card * (pos + 1) }.sum
+    def rescursive: Boolean = deck.tail.size >= deck.head
   }
 
   case class Game(player1: Player, player2: Player) {
@@ -23,7 +25,30 @@ object Day22 {
       (Game(p1, p2), c1, c2)
     }
 
-    def gameover: Boolean = player1.left == 0 || player2.left == 0
+    def nextRecursive: Game = {
+      val (p1, c1) = player1.take
+      val (p2, c2) = player2.take
+
+      val deck1 = p1.deck.take(c1)
+      val deck2 = p2.deck.take(c2)
+
+      Game(Player(deck1), Player(deck2))
+    }
+
+    def gameover: Boolean = winner1 || winner2
+
+    def recursive: Boolean = 
+      player1.rescursive && player2.rescursive
+
+    def winner1: Boolean = player2.looser
+
+    def winner2: Boolean = player1.looser
+
+    def updateP1(card1: Card, card2: Card): Game = 
+      update((p1, p2) => (p1.add(card1, card2), p2))
+
+    def updateP2(card1: Card, card2: Card): Game = 
+      update((p1, p2) => (p1, p2.add(card1, card2)))
 
     def update(f: (Player, Player) => (Player, Player)): Game = {
       val (p1, p2) = f(player1, player2)
@@ -45,13 +70,40 @@ object Day22 {
       game
     else {
       val next = game.next match {
-        case (next, card1, card2) if card1 > card2 => next.update { case (p1, p2) => (p1.add(card1, card2), p2)}
-        case (next, card1, card2) if card1 < card2 => next.update { case (p1, p2) => (p1, p2.add(card2, card1))}
+        case (next, card1, card2) if card1 > card2 => next.updateP1(card1, card2)
+        case (next, card1, card2) if card1 < card2 => next.updateP2(card2, card1)
         case (next, card1, card2) if card1 == card2 => throw new IllegalStateException(s"$card1,$card2")
       }
       play(next)
     }
   }
+
+  type State = (Set[(Card, Card)], Game)
+
+  def play2(state: State): Game = 
+    state match {
+      case (played, game) =>
+        if (game.gameover)
+          game
+        else if (game.recursive) {
+          val result = play2(played, game.nextRecursive)
+          
+          val next = game.next match {
+            case (next, card1, card2) if result.winner1 => next.updateP1(card1, card2)
+            case (next, card1, card2) if result.winner2 => next.updateP2(card2, card1)
+            case (next, card1, card2) => throw new IllegalStateException(s"$card1,$card2")
+          }
+
+          play2(played, next)
+        } else {
+          val next = game.next match {
+            case (next, card1, card2) if card1 > card2 => next.updateP1(card1, card2)
+            case (next, card1, card2) if card1 < card2 => next.updateP2(card2, card1)
+            case (next, card1, card2) => throw new IllegalStateException(s"$card1,$card2")
+          }
+          play2(played, next)
+        }
+    }
 }
 
 object Day22Part1 extends App {
@@ -64,13 +116,15 @@ object Day22Part1 extends App {
 
   val result = play(Game(player1, player2))
   println(result.winner.map(_.points))
+  val result2 = play2(Set.empty, Game(player1, player2))
+  println(result2.winner.map(_.points))
 }
 
 object Day22Test extends App {
   import Day22._
 
-  val p1 = Player(List(9,2,6,3,1))
-  val p2 = Player(List(5,8,4,7,10))
+  val p1 = Player(List(9, 2, 6, 3, 1))
+  val p2 = Player(List(5, 8, 4, 7, 10))
   val game = Game(p1, p2)
 
   val result = play(game)
@@ -80,6 +134,9 @@ object Day22Test extends App {
   val winner = result.winner.orNull
 
   assert(winner.points == 306)
+
+  val winner2 = play2(Set.empty, game).winner.orNull
+  assert(winner2.points == 291)
 
   println("OK")
 }
