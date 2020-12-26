e45a8b9aac6f26bcb4862c188d9eaf67b253ebc2
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-26 16:54:09 +0100

    Day23 part1 using an array pointing the next element

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -1,6 +1,7 @@
 package adventofcode
 
 import scala.annotation.tailrec
+import scala.collection.parallel.immutable.ParVector
 
 object Day23 {
 
@@ -10,52 +11,81 @@ object Day23 {
     b
   }
 
-  case class CircularList(list: List[Int]) {
+  /*
+   * Vector will contain on each position the value of the next element
+   * 
+   * For example: 3, 8, 9, 1, 2, 5, 4, 6, 7
+   *  
+   * |0|1|2|3|4|5|6|7|8|9|
+   * ---------------------
+   * |3|2|5|8|6|4|7|3|9|1|
+   * 
+   * Next step will be: 2, 8, 9, 1, 5, 4, 6, 7, 3
+   *  
+   * |0|1|2|3|4|5|6|7|8|9|
+   * ---------------------
+   * |2|5|8|2|6|4|7|3|9|1|
+   * 
+   */
+  case class CircularList(list: Vector[Int]) {
+
+    def toVector: Vector[Int] = take(0, list.size - 1)
 
     def move: CircularList = {
-      val current = list.head
-      val (taken, rest) = take(3)
-      val destination = search(current - 1, rest)
+      val current = list(0)
+      val taken = take(current, 3)
+      var target = search(current - 1, taken).orElse(search(list.size - 1, taken)).getOrElse(0)
 
-      val (a, b) = rest.splitAt(destination)
+      assert(target > 0, s"invalid target: $target")
+
+      val head = takeWhile(taken.last, target)
+      val tail = takeWhile(target, current)
+      val result = (head :+ target) ++ taken ++ (tail :+ current)
+
+      CircularList(result:_*)
+    }
       
-      val res = ((a :+ b.head) ++ taken ++ b.tail) :+ current
-
-      CircularList(res)
-    }
-
-    def take(n: Int): (List[Int], List[Int]) = 
-      (list.tail.take(n), list.drop(n).tail)
-
-    def search(target: Int, rest: List[Int]): Int =
-      if (target < 1)
-        rest.indexOf(rest.max)
+    def takeWhile(from: Int, target: Int): Vector[Int] = {
+      val next = list(from)
+      if (next != target) {
+        next +: takeWhile(next, target) 
+      }
       else 
-        rest.indexOf(target) match {
-          case x if x < 0 => search(target - 1, rest)
-          case x => x
-        }
-
-    def label: String = {
-      val x = list.indexOf(1)
-
-      val y = circular(list).drop(x + 1).take(list.size - 1).toList
-
-      y.mkString
+        Vector.empty
     }
+
+    def take(from: Int, n: Int): Vector[Int] = 
+      if (n > 0) {
+        val current = list(from)
+        current +: take(current, n - 1) 
+      }
+      else 
+        Vector.empty
+
+    def search(target: Int, taken: Vector[Int]): Option[Int] =
+      if (target < 1)
+        None
+      else if (taken.contains(target))
+          search(target - 1, taken)
+        else
+          Some(target)
+
+    def label: String = take(1, list.size - 2).mkString
   }
 
   object CircularList {
-    def apply(items: Int*): CircularList = CircularList(List(items:_*))
 
-    def build(items: Int*): CircularList = {
-      val start = List(items:_*)
+    def apply(items: Int*): CircularList = {
+      val (res, last) = items.foldLeft((Array.fill(items.size + 1)(0), 0)) {
+        case ((array, previous), current) =>
+          array(previous) = current
+          (array, current)
+      }
 
-      val rest = for {
-        x <- (start.max until 1000000)
-      } yield(x)
+      // last should point to first to complete the circle
+      res(last) = items.head
 
-      CircularList(start ++ rest)
+      CircularList(res.toVector)
     }
   }
 
@@ -79,7 +109,12 @@ object Day23Part1 extends App {
 object Day23Test extends App {
   import Day23._
 
+  // 3,2,5,8,6,4,7,3,9,1
+  // 3,2,5,8,6,4,7,0,9,1
   val list1 = CircularList(3, 8, 9, 1, 2, 5, 4, 6, 7)
+
+  assert(list1.list == Vector(3, 2, 5, 8, 6, 4, 7, 3, 9, 1))
+  assert(list1.toVector == Vector(3, 8, 9, 1, 2, 5, 4, 6, 7))
 
   assert(list1.move == CircularList(2, 8, 9, 1, 5, 4, 6, 7, 3))
   assert(list1.move.move == CircularList(5, 4, 6, 7, 8, 9, 1, 3, 2))
@@ -92,8 +127,8 @@ object Day23Test extends App {
   val result100 = play(100)(list1)
   assert(result100.label == "67384529")
 
-//  val result10M = play(10000000)(CircularList.build(3, 8, 9, 1, 2, 5, 4, 6, 7))
-//  println(result10M)
+  //val result10M = play(10000000)(CircularList(1000000)(3, 8, 9, 1, 2, 5, 4, 6, 7))
+  //println(result10M)
 
   println("OK")
 }
