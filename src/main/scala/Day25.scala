256f105a69282a20258a07ee03e4ccf3e494e113
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-27 23:50:04 +0100

    Day25 part1 solution

--- src/main/scala/Day25.scala
+++ src/main/scala/Day25.scala
@@ -1,36 +1,60 @@
 package adventofcode
 
 import scala.annotation.tailrec
+import scala.io.Source
 
 object Day25 {
 
-  val subject = 7
-  val magic = 20201227
+  val magic = 20201227L
 
-  def transform(x: Int) = ???
-
-  def step(x: Int): Int = (x * subject) % magic
+  def step(x: Long, subject: Long): Long = (x * subject) % magic
 
   @tailrec
-  def loop(n: Int, x: Int = 1): Int = 
+  def loop(n: Long, x: Long = 1)(subject: Long): Long = 
     if (n > 0) 
-      loop(n - 1, step(x)) 
+      loop(n - 1, step(x, subject))(subject)
     else x
+
+  def bruteforce(key: Long): Option[Long] = {
+    Stream.iterate(1L)(step(_, 7))
+      .zipWithIndex
+      .find(_._1 == key)
+      .map(_._2)
+  }
+}
+
+object Day25Part1 extends App {
+  import Day25._
+
+  val Seq(card, door) = Source.fromResource("keys.txt").getLines().map(_.toLong).toSeq
+
+  val loopSizeCard = bruteforce(card).get
+
+  // 16311885
+  println(loop(loopSizeCard)(door))
 }
 
 object Day25Test extends App {
   import Day25._
+  
+  val subject = 7
 
-  val doorPubKey = 17807724
-  val cardPubKey = 5764801
+  val doorPubKey = 17807724L
+  val cardPubKey = 5764801L
 
-  assert(loop(8) == cardPubKey)
-  assert(loop(11) == doorPubKey)
+  val secretKey = 14897079
 
-  println(loop(8, doorPubKey))
-  println(loop(11, cardPubKey))
+  val doorLoopSize = bruteforce(doorPubKey).get
+  val cardLoopSize = bruteforce(cardPubKey).get
 
-  assert(transform(cardPubKey) == transform(doorPubKey))
+  assert(doorLoopSize == 11)
+  assert(cardLoopSize == 8)
+
+  assert(loop(cardLoopSize)(subject) == cardPubKey)
+  assert(loop(doorLoopSize)(subject) == doorPubKey)
+
+  assert(loop(cardLoopSize)(doorPubKey) == secretKey)
+  assert(loop(doorLoopSize)(cardPubKey) == secretKey)
 
   println("OK")
 }
