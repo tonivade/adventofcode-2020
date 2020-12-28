8ddc5eda8bbdf196a22b2f1ad94e083476cfa79a
Antonio Muñoz <antoniogmc@gmail.com>
2020-12-28 12:31:44 +0100

    Day20 part2 WIP

--- src/main/scala/Day20.scala
+++ src/main/scala/Day20.scala
@@ -1,8 +1,15 @@
 package adventofcode
 
 import scala.io.Source
+import scala.collection.mutable
+import scala.collection.SortedSet
+import scala.collection.mutable.HashSet
 
 object Day20 {
+
+  implicit class ChainOps[A](val value: A) extends AnyVal {
+    def |>[B](f: A => B): B = f(value)
+  }
 
   case class Tile(id: Int, image: Seq[String]) {
     def top: String = image.head
@@ -26,7 +33,8 @@ object Day20 {
   }
 
   case class Result(id: Int, top: Option[Int], botton: Option[Int], left: Option[Int], right: Option[Int]) {
-    def positives: Int = List(top,botton,left,right).flatMap(_.toList).size
+    def matches: Seq[Int] = Seq(top, botton, left, right).flatMap(_.toList)
+    def count: Int = matches.size
   }
 
   def parse(input: String): Seq[Tile] = 
@@ -54,8 +62,7 @@ object Day20 {
     tiles.filterNot(_.id == tile.id).find(_.bottom == tile.top).map(_.id)
   }
 
-  def findCorners(input: String): Seq[Int] = {
-    val tiles = parse(input)
+  def findMatches(tiles: Seq[Tile]): Map[Int, Result] = {
     val rotated = tiles.map(_.rotate)
     val rnf = rotated.map(_.flip)
     val flipped = tiles.map(_.flip)
@@ -75,23 +82,100 @@ object Day20 {
     }
 
     result.groupBy(_.id)
-      .map(x => (x._1, x._2.maxBy(_.positives)))
-      .filter(_._2.positives == 2)
-      .map(_._1)
-      .toSeq
+      .map(x => (x._1, x._2.maxBy(_.count)))
+      .toMap
   }
+
+  def part1(input: Map[Int, Result]): Long =
+    input.values.filter(_.count == 2)
+      .map(_.id).foldLeft(1L)(_ * _)
+
+  sealed trait Move {
+    def turn: Move
+    def reverse: Move
+    def next(input: Result): Option[Int]
+  }
+  case object Top extends Move {
+    override def turn: Move = Right
+    override def reverse: Move = Bottom
+    override def next(input: Result): Option[Int] = input.top
+  }
+  case object Bottom extends Move {
+    override def turn: Move = Left
+    override def reverse: Move = Top
+    override def next(input: Result): Option[Int] = input.botton
+  }
+  case object Left extends Move {
+    override def turn: Move = Top
+    override def reverse: Move = Right
+    override def next(input: Result): Option[Int] = input.left
+  }
+  case object Right extends Move {
+    override def turn: Move = Bottom
+    override def reverse: Move = Left
+    override def next(input: Result): Option[Int] = input.right
+  }
+
+  def search(input: Result, matches: Map[Int, Result]): (Int, Set[List[Int]]) = {
+
+    val size = Math.sqrt(matches.size).toInt
+
+    val memory = HashSet.empty[List[Int]]
+
+    def follow(move: Move)(input: Int, path: List[Int]): List[Int] = {
+      matches.get(input).flatMap(move.next) match {
+        case Some(value) => 
+          if (path.contains(value))
+            // loop detected
+            follow(move.turn)(value, path.takeWhile(_ != value))
+          else
+            follow(move)(value, path :+ value)
+        case None => 
+          if (path.size < size)
+            // not completed
+            follow(move.turn)(input, path)
+          else if (path.size == size)
+            // success
+            path
+          else
+            Nil
+        }
+    }
+
+    val paths = Seq(Top, Bottom, Left, Right)
+      .map(follow(_)(input.id, List(input.id))).filterNot(_.isEmpty)
+      .toSet
+
+    (input.id, paths)
+  }
+
+  val input = Source.fromResource("tiles.txt").mkString
 }
 
 object Day20Part1 extends App {
   import Day20._
 
-  val input = Source.fromResource("tiles.txt").mkString
-
-  val corners = findCorners(input)
-
-  println(corners)
+  val result = parse(input) |> findMatches |> part1
   
-  println(corners.foldLeft(1L)(_ * _))
+  println(result)
+}
+
+object Day20Part2 extends App {
+  import Day20._
+  
+  val tiles = parse(input)
+
+  val matches = findMatches(tiles)
+
+  val Seq(corner1, corner2, corner3, corner4) = 
+    matches.values.filter(_.count == 2).toSeq
+
+  println(search(corner1, matches))
+//  println(search(corner2, matches))
+//  println(search(corner3, matches))
+//  println(search(corner4, matches))
+  
+  matches.values.filter(_.count == 3).foreach(println)
 }
 
 object Day20Test extends App {
@@ -109,6 +193,8 @@ object Day20Test extends App {
   assert(x.rotate.right == x.bottom)
   assert(y.flip.left == y.right)
   assert(y.flip.right == y.left)
+  assert(x.invert.left == x.right.reverse)
+  assert(x.invert.right == x.left.reverse)
 
   val input = """Tile 2311:
                 |..##.#..#.
@@ -218,11 +304,17 @@ object Day20Test extends App {
                 |..#.......
                 |..#.###...""".stripMargin
 
-  val corners = findCorners(input)
+  val matches = parse(input) |> findMatches
 
-  assert(corners.size == 4)
+  assert(part1(matches) == 20899048083289L)
 
-  assert(corners.foldLeft(1L)(_ * _) == 20899048083289L)
+  val Seq(corner1, corner2, corner3, corner4) = 
+    matches.values.filter(_.count == 2).toSeq
+
+  println(search(corner1, matches))
+  println(search(corner2, matches))
+  println(search(corner3, matches))
+  println(search(corner4, matches))
 
   println("OK")
 }
